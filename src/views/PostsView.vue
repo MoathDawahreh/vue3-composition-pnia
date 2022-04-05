709f36385019b911e9b43e0e7a2973f607fe552e
MoathDawahreh <moath.dawahreh@gmail.com>
2022-04-06 01:17:49 +0300

    missing around

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -1,33 +1,28 @@
 <template>
-  <div class="posts">
-    <h1> posts </h1>
-    <div class="posts-list">
-      <ul>
-        <li v-for="post in posts" :key="post.id">
-          <RouterLink :to="{ name: 'postDetail', params: { id: post.id }}"
->
-            {{ post.title }}
-          </RouterLink>
-        </li>
-      </ul>
-    <textarea></textarea>
-  </div>
-  </div>
+	<div class="posts">
+		<h1>posts</h1>
+		<div class="posts-list">
+			<ul>
+				<li v-for="post in posts" :key="post.id">
+					<RouterLink :to="{ name: 'postDetail', params: { id: post.id } }">
+						{{ post.title }}
+					</RouterLink>
+				</li>
+			</ul>
+			<textarea></textarea>
+		</div>
+	</div>
 </template>
 <script setup lang="ts">
-import { isInDestructureAssignment, walkBlockDeclarations } from "@vue/compiler-core";
- const posts = [
-  {
-    id: 1,
-    title: "Hello World",
-  },
-  {
-    id: 2,
-    title: "Bye World",
-  },
-];
- 
+import { ref } from 'vue'
+import {
+	isInDestructureAssignment,
+	walkBlockDeclarations,
+} from '@vue/compiler-core'
+
+const posts = ref([
+	{ id: 1, title: 'first post' },
+	{ id: 2, title: 'second post' },
+	{ id: 3, title: 'third post' },
+])
 </script>
-
-
- 
